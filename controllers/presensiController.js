bb9a1517f875118074852aa6269b0d89fb0498a2
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-10-31 06:21:14 +0700

    Tambahkan validasi express-validator untuk waktuCheckIn dan waktuCheckOut pada endpoint PUT /api/presensi/:id

--- controllers/presensiController.js
+++ controllers/presensiController.js
@@ -104,8 +104,7 @@
 			 .status(403)
 			 .json({message: "Akses ditolak: Anda bukan pemilik catatan ini."});
 		}
-
-		await recordToDelete.destroy();
+	await recordToDelete.destroy();
 		res.status(204).send();
 	  } catch (error) {
 		res
@@ -113,15 +112,14 @@
 		 .json({message: "Terjadi kesalahan pada server", error:error.message});
 	  }
 	};
-
 	exports.updatePresensi = async (req, res) => {
       try {
 		const presensiId = req.params.id;
 		const { checkIn, checkOut, nama } = req.body;
 		if (checkIn === undefined && checkOut === undefined && nama === undefined) {
-		return res.status(400).json({
-			message:
-			"Request body tidak berisi data yang valid untuk diupdate (checkIn, checkOut, atau nama).",
+			return res.status(400).json({
+				message:
+					"Request body tidak berisi data yang valid untuk diupdate (checkIn, checkOut, atau nama).",
 		});
 		}
 		const recordToUpdate = await Presensi.findByPk(presensiId);
