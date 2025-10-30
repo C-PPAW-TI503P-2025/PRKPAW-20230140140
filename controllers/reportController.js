bab0892f7ee0ef15cb2298ef88312ac3296840b4
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-10-30 12:50:37 +0700

    Selesaikan konflik merge antara lokal dan GitHub

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -20,3 +20,29 @@ exports.getDailyReport = async (req, res) => {
     });
   }
 };
+const {Op} = require("../models");
+
+exports.getDailyReport = async (req, res) => {
+  try {
+    const { nama } = req.query;
+    let options = { where: {} };
+
+    if (nama) {
+      options.where.nama = {
+        [Op.like]: `%${nama}%`,
+      };
+    }
+
+    const records = await Presensi.findAll(options);
+
+    res.json({
+      reportDate: new Date().toLocaleDateString(),
+      data: records,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Gagal mengambil laporan", error: error.message });
+  }
+};
+
--- tugas4.md
+++ tugas4.md
@@ -16,3 +16,17 @@ tugas 4
 
 5. Tampilan Endpoint reports/daily (Karyawan)
 ![Tampilan Endpoint reports/daily](ss/report2.png)
+1. 
+![Tampilan Endpoint presensi/check-in](ss/EndPointCheck-In(tugas4).png)
+
+2.
+![Tampilan Endpoint presensi/check-out](ss/EndPointCheck-In(tugas4).png)
+
+3.
+![Tampilan Database](ss/database.png)
+
+4.
+![Tampilan Endpoint reports/daily](ss/EndPointReport(tugas4).png)
+
+5.
+![Tampilan Endpoint reports/daily](ss/report2.png)
